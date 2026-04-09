928ecf2e40c436737c4302a3b31af0e3470a08e6
Luca <luccasbazza@gmail.com>
2026-04-08 21:56:49 -0300

    feat: Add package-lock.json and update matrix rain script formatting

--- js/matrix-rain.js
+++ js/matrix-rain.js
@@ -1,29 +1,31 @@
-const canvas = document.getElementById('matrix-canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("matrix-canvas");
+const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ1234567890';
-letters = letters.split('');
+let letters =
+  "ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ1234567890";
+letters = letters.split("");
 
 let fontSize = 10,
-    columns = canvas.width / fontSize;
+  columns = canvas.width / fontSize;
 
 let drops = [];
 for (let i = 0; i < columns; i++) {
-  drops[i] = Math.floor(Math.random() * canvas.height / fontSize);
+  drops[i] = Math.floor((Math.random() * canvas.height) / fontSize);
 }
 
 function draw() {
-  ctx.fillStyle = 'rgba(13, 13, 13, .1)';
+  ctx.fillStyle = "rgba(13, 13, 13, .1)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   for (let i = 0; i < drops.length; i++) {
     let text = letters[Math.floor(Math.random() * letters.length)];
-    ctx.fillStyle = '#00ff41';
+    ctx.fillStyle = "#00ff41";
     ctx.fillText(text, i * fontSize, drops[i] * fontSize);
     drops[i]++;
-    if (drops[i] * fontSize > canvas.height && Math.random() > .975) { // Aumentado para un reseteo más esporádico
+    if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+      // Aumentado para un reseteo más esporádico
       drops[i] = 0;
     }
   }
@@ -33,12 +35,12 @@ function draw() {
 setInterval(draw, 33);
 
 // Redimensionar el canvas si la ventana cambia
-window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    columns = canvas.width / fontSize;
-    drops = [];
-    for (let i = 0; i < columns; i++) {
-        drops[i] = Math.floor(Math.random() * canvas.height / fontSize);
-    }
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  columns = canvas.width / fontSize;
+  drops = [];
+  for (let i = 0; i < columns; i++) {
+    drops[i] = Math.floor((Math.random() * canvas.height) / fontSize);
+  }
 });
